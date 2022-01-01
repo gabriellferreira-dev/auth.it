faf251bf9a8af86fc67ad5b85f5476f94e8ab846
Gabriel L Ferreira <78182689+gabriellferreira-dev@users.noreply.github.com>
2022-01-01 17:43:57 -0300

    feat: user password encrypt

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,10 +1,13 @@
 import {
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
   ObjectIdColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import bcrypt from 'bcryptjs';
 
 @Entity()
 class User {
@@ -38,6 +41,12 @@ class User {
     nullable: true,
   })
   updatedAt!: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
 
 export default User;
